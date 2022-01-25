878a9463dc6b085ab078c9589e91314bf474a337
Muhtasim Fahim <muhtasim.fahim.98@gmail.com>
2022-01-25 20:07:54 +0600

    'fav-writter-bug-fixed'

--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
     name: {type: String, required: true},
     image: {type: String, required: true},
     email: {type: String, required: true},
-    fav_writter: {type: String, required: true},
+    fav_writter: {type: String},
     address: {type: String, required: true},
     contact: {type: String, required: true},
     dob: {type: Date, required: true},
